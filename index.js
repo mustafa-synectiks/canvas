79d92340398687d9f680f7f05c155eb77a0d8f59
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2020-07-02 15:48:39 +0530

    code committed

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-	world,
-	MouseConstraint.create(engine, {
-		mouse: Mouse.create(render.canvas)
-	})
-);
+// World.add(
+// 	world,
+// 	MouseConstraint.create(engine, {
+// 		mouse: Mouse.create(render.canvas)
+// 	})
+// );
 const walls = [
 	Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
 	Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
@@ -31,10 +31,10 @@ const walls = [
 ];
 World.add(world, walls);
 
-for (let i = 0; i < 20; i++) {
-	if (Math.random() > 0.5) {
-		World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
-	} else {
-		World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 30));
-	}
-}
+// for (let i = 0; i < 20; i++) {
+// 	if (Math.random() > 0.5) {
+// 		World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
+// 	} else {
+// 		World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 30));
+// 	}
+// }
